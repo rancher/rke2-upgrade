2afb96e51df5c33b4ef6eece2c5d035170ec3851
Brad Davidson <brad.davidson@rancher.com>
2025-11-18 18:03:49 +0000

    Migrate from master to control-plane labels
    Basically the RKE2 version of https://github.com/k3s-io/k3s-upgrade/pull/57
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -13,13 +13,23 @@ fatal()
 
 get_rke2_process_info() {
   RKE2_PID=$(ps -ef | grep -E "(/usr|/usr/local|/opt/rke2)/bin/rke2 .*(server|agent)" | grep -E -v "(init|grep)" | awk '{print $1}')
+
   if [ -z "$RKE2_PID" ]; then
     fatal "rke2 is not running on this server"
   fi
+
+  if [ "$(echo $RKE2_PID | wc -w)" != "1" ]; then
+    for PID in $RKE2_PID; do
+      ps -fp $PID || true
+    done
+    fatal "Found multiple rke2 pids"
+  fi
+
   info "rke2 binary is running with pid $RKE2_PID"
-  RKE2_BIN_PATH=$(cat /host/proc/${RKE2_PID}/cmdline | awk '{print $1}' | head -n 1)
+
+  RKE2_BIN_PATH=$(awk 'NR==1 {print $1}' /host/proc/${RKE2_PID}/cmdline)
   if [ -z "$RKE2_BIN_PATH" ]; then
-    fatal "Failed to fetch the rke2 binary path from process $RKE2_PID"
+    fatal "Failed to fetch the rke2 binary path from pid $RKE2_PID"
   fi
   return
 }
@@ -27,18 +37,28 @@ get_rke2_process_info() {
 replace_binary() {
   NEW_BINARY="/opt/rke2"
   FULL_BIN_PATH="/host$RKE2_BIN_PATH"
-  if [ ! -f $NEW_BINARY ]; then
+
+  if [ ! -f "$NEW_BINARY" ]; then
     fatal "The new binary $NEW_BINARY doesn't exist"
   fi
+
   info "Comparing old and new binaries"
-  BIN_COUNT="$(sha256sum $NEW_BINARY $FULL_BIN_PATH | cut -d" " -f1 | uniq | wc -l)"
-  if [ $BIN_COUNT == "1" ]; then
+  BIN_CHECKSUMS="$(sha256sum $NEW_BINARY $FULL_BIN_PATH)"
+
+  if [ "$?" != "0" ]; then
+    fatal "Failed to calculate binary checksums"
+  fi
+
+  BIN_COUNT="$(echo "${BIN_CHECKSUMS}" | awk '{print $1}' | uniq | wc -l)"
+  if [ "$BIN_COUNT" == "1" ]; then
     info "Binary already been replaced"
     exit 0
   fi
+
   RKE2_CONTEXT=$(getfilecon $FULL_BIN_PATH 2>/dev/null | awk '{print $2}' || true)
   info "Deploying new rke2 binary to $RKE2_BIN_PATH"
   cp $NEW_BINARY $FULL_BIN_PATH
+
   if [ -n "${RKE2_CONTEXT}" ]; then
     info 'Restoring rke2 bin context'
     setfilecon "${RKE2_CONTEXT}" $FULL_BIN_PATH
@@ -62,48 +82,50 @@ kill_rke2_process() {
     # to automatically restart rke2 with the new version
     CHILD_PIDS=$(pgrep -lP $RKE2_PID | grep -Eo '[0-9]+ (containerd|kubelet)' | awk 'BEGIN{ORS=" "}{print $1}')
     kill -SIGTERM $RKE2_PID $CHILD_PIDS
-    info "Successfully Killed old rke2 process $RKE2_PID and containerd/kubelet processes $CHILD_PIDS"
+    info "Successfully killed old rke2 pid $RKE2_PID and containerd/kubelet pid $CHILD_PIDS"
 }
 
 prepare() {
   set +e
-  MASTER_PLAN=${1}
-  if [ -z "$MASTER_PLAN" ]; then
-    fatal "Master Plan name is not passed to the prepare step. Exiting"
+  CONTROLPLANE_PLAN=${1}
+
+  if [ -z "$CONTROLPLANE_PLAN" ]; then
+    fatal "Control-plane Plan name is not passed to the prepare step. Exiting"
   fi
+
   NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
   while true; do
-    # make sure master plan does exist
-    PLAN=$(kubectl get plan $MASTER_PLAN -o jsonpath='{.metadata.name}' -n $NAMESPACE 2>/dev/null)
+    # make sure control-plane plan does exist
+    PLAN=$(kubectl get plan $CONTROLPLANE_PLAN -o jsonpath='{.metadata.name}' -n $NAMESPACE 2>/dev/null)
     if [ -z "$PLAN" ]; then
-	    info "master plan $MASTER_PLAN doesn't exist"
+	    info "Waiting for control-plane Plan $CONTROLPLANE_PLAN to be created"
 	    sleep 5
 	    continue
     fi
-    NUM_NODES=$(kubectl get plan $MASTER_PLAN -n $NAMESPACE -o json | jq '.status.applying | length')
+    NUM_NODES=$(kubectl get plan $CONTROLPLANE_PLAN -n $NAMESPACE -o json | jq '.status.applying | length')
     if [ "$NUM_NODES" == "0" ]; then
       break
     fi
-    info "Waiting for all master nodes to be upgraded"
+    info "Waiting for all control-plane nodes to be upgraded"
     sleep 5
   done
-  verify_masters_versions
+  verify_controlplane_versions
 }
 
-verify_masters_versions() {
+verify_controlplane_versions() {
   while true; do
     all_updated="true"
-    MASTER_NODE_VERSION=$(kubectl get nodes --selector='node-role.kubernetes.io/master' -o json | jq -r '.items[].status.nodeInfo.kubeletVersion' | sort -u | tr '+' '-')
-    if [ -z "$MASTER_NODE_VERSION" ]; then
+    CONTROLPLANE_NODE_VERSION=$(kubectl get nodes --selector='node-role.kubernetes.io/control-plane' -o json | jq -r '.items[].status.nodeInfo.kubeletVersion' | sort -u | tr '+' '-')
+    if [ -z "$CONTROLPLANE_NODE_VERSION" ]; then
       sleep 5
       continue
     fi
     K8S_IMAGE_TAG=$(bash /bin/semver-parse.sh $SYSTEM_UPGRADE_PLAN_LATEST_VERSION k8s)
-    if [ "$MASTER_NODE_VERSION" == "$K8S_IMAGE_TAG" ]; then
-        info "All master nodes has been upgraded to version to $MASTER_NODE_VERSION"
+    if [ "$CONTROLPLANE_NODE_VERSION" == "$K8S_IMAGE_TAG" ]; then
+        info "All control-plane nodes have been upgraded to version to $CONTROLPLANE_NODE_VERSION"
 		    break
 		fi
-    info "Waiting for all master nodes to be upgraded to version $MODIFIED_VERSION"
+    info "Waiting for all control-plane nodes to be upgraded to version $MODIFIED_VERSION"
 	  sleep 5
 	  continue
   done
